7f05696cbc30bbab1802d6cb4051e6547d249dbe
amitappbuilder <amit.appbuilder@gmail.com>
2025-01-21 19:29:28 -0500

    Initial commit from your app

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,8 +1,8 @@
 import { EventData, Page } from '@nativescript/core';
-import { HelloWorldModel } from './main-view-model';
-
-export function navigatingTo(args: EventData) {
-  const page = <Page>args.object;
-
-  page.bindingContext = new HelloWorldModel();
-}
+    
+    
+    export function navigatingTo(args: EventData) {
+      const page = <Page>args.object;
+    
+      
+    }
